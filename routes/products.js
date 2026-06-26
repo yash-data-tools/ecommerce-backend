9e3941fa9a4a4f79c9a60ade92e63f34095b89b6
Yash <163550213+yash-data-tools@users.noreply.github.com>
2026-06-26 15:45:51 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -24,11 +24,7 @@ router.get('/', async (req, res) => {
   } else {
     products = await Product.findAll();
   }
-  const BASE_URL = "https://ecommerce-backend-4e2u.onrender.com";
-  products = products.map(product =>({
-    ...product,
-    image: `${BASE_URL}/${product.image}`
-  }));
+
   res.json(products);
 });
 
